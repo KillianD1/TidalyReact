482781d53bb008bbc72c2cbecd3f2c894bd42d0b
KillianD1 <72009583+KillianD1@users.noreply.github.com>
2023-04-16 12:05:57 +0200

    feat: add AccountSetup view and css for the view

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Login } from './Login';
 import { Register } from './Register';
+import { AccountSetup } from "./AccountSetup";
 
 function App() {
 
